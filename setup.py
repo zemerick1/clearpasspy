77928c0815e06213341c719d6cd784feac851ed7
zemerick <zemerick@emerickcc.com>
2019-03-07 19:48:32 -0600

    bumped version to 1.1.0

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clearpasspy",
+    version="1.1.0",
+    author="zemerick1",
+    author_email="zemerick@emerickcc.com",
+    description="ClearPass API Python Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zemerick1/clearpasspy",
+    packages=setuptools.find_packages(),
+    py_modules=['clearpasspy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
