5734b3516f1291a7b8fcd9b58d8be40326ea883b
zemerick <zemerick@emerickcc.com>
2019-03-07 20:00:12 -0600

    bumped code for setup.py requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clearpasspy",
-    version="1.1.0",
+    version="1.1.1",
     author="zemerick1",
     author_email="zemerick@emerickcc.com",
     description="ClearPass API Python Library",
