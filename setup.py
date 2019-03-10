2148757841efcfa7af55f6a173565096acab38f7
zemerick <zemerick@emerickcc.com>
2019-03-09 19:43:05 -0600

    bumped ver to 1.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="clearpasspy",
-    version="1.1.2",
+    version="1.1.3",
     author="zemerick1",
     author_email="zemerick@emerickcc.com",
     description="ClearPass API Python Library",
